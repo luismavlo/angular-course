52fd050187e4a414c2605087477393004b55b1de
Luis Miguel <lmavendano1597@gmail.com>
2023-09-08 09:58:58 -0500

    primera parte curso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'semaforo';
+  public count: number = 0;
+
+  increase() {
+    if (this.count >= 3) return;
+    this.count++;
+  }
+
+  decrease() {
+    if (this.count <= 0) return;
+    this.count--;
+  }
 }
